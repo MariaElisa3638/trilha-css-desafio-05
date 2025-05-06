3df3593247770c8ff024fd660ac6dd14ad87998e
mariaelisarf <maria.ferreira@sicoobcredimata.com.br>
2025-05-06 13:40:25 -0300

    adiciona implementação da classe Pokemon e funções para requisição à API do Pokedex

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,24 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-fetch(url).then(function(response){
-console.log(response);
-}).catch(function(){
-console.error(error)
-}).finally(function(){
-console.log('Requisição finalizada')
+
+function convertPokemonToLi(pokemon) {
+    return `  <li class="pokemon">
+                <span class="number">${pokemon.number}1</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="details">
+                    <ol class="types">
+                 ${pokemon.types.map((type)=>'<li class="type">${type}</li>').join('')}
+                    </ol>
+                       <img src="${pokemon.photo}"
+                        alt="${pokemon.name}">
+                      
+                </div>
+            </li>`
+}
+
+const pokemonList = document.getElementById('pokemons');
+pokeApi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('');
 })
 
-const x=20;
-console.log(x);
